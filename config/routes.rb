f5a57dcd591ac872332ecb5a3f1ef668d18c7bdb
yvan poussard <yvs.pro64@gmail.com>
2020-03-05 14:50:46 +0100

    rake ok

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,8 @@
 
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :doses, only: :destroy
+  resources :cocktails, only: %i[index show new create] do
+    resources :doses, only: %i[new create]
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,5 @@
 # frozen_string_literal: true
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
@@ -6,13 +7,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-require "open-uri"
-require "json"
+require 'open-uri'
+require 'json'
 
-url = "https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list"
+url = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
 
-cocktails = JSON.parse(open(url).read)
+ingredients = JSON.parse(open(url).read)
 
-cocktails["drinks"].each do |ingredient|
-  Ingredient.create(name: "#{ingredient["strIngredient1"]}")
+ingredients['drinks'].each do |ingredient|
+  Ingredient.create(name: (ingredient['strIngredient1']).to_s)
 end
