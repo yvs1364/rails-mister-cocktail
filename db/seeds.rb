cb7684f8564f3e4504866a0b4eafe939c390fb21
yvan poussard <yvs.pro64@gmail.com>
2020-03-05 12:01:34 +0100

    seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require "open-uri"
+require "json"
+
+url = "https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list"
+
+cocktails = JSON.parse(open(url).read)
+
+cocktails["drinks"].each do |ingredient|
+  Ingredient.create(name: "#{ingredient["strIngredient1"]}")
+end
