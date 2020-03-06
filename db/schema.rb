51d3e8cb85a8b01dd4ba41dca8066f298f054db2
yvan poussard <yvs.pro64@gmail.com>
2020-03-06 16:44:39 +0100

    root heroku

--- db/schema.rb
+++ db/schema.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -12,32 +10,33 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20_200_305_102_850) do
+ActiveRecord::Schema.define(version: 2020_03_05_102850) do
+
   # These are extensions that must be enabled in order to support this database
-  enable_extension 'plpgsql'
+  enable_extension "plpgsql"
 
-  create_table 'cocktails', force: :cascade do |t|
-    t.string 'name'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
+  create_table "cocktails", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
-  create_table 'doses', force: :cascade do |t|
-    t.string 'description'
-    t.bigint 'cocktail_id'
-    t.bigint 'ingredient_id'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.index ['cocktail_id'], name: 'index_doses_on_cocktail_id'
-    t.index ['ingredient_id'], name: 'index_doses_on_ingredient_id'
+  create_table "doses", force: :cascade do |t|
+    t.string "description"
+    t.bigint "cocktail_id"
+    t.bigint "ingredient_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["cocktail_id"], name: "index_doses_on_cocktail_id"
+    t.index ["ingredient_id"], name: "index_doses_on_ingredient_id"
   end
 
-  create_table 'ingredients', force: :cascade do |t|
-    t.string 'name'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
+  create_table "ingredients", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
-  add_foreign_key 'doses', 'cocktails'
-  add_foreign_key 'doses', 'ingredients'
+  add_foreign_key "doses", "cocktails"
+  add_foreign_key "doses", "ingredients"
 end
